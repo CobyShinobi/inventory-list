0402401d3cd5c9c83390055b44a23d1bf7c82eeb
Coby Leavitt <coby.leavitt@gmail.com>
2021-05-22 15:33:49 -0600

    added index.ejs and basic submission form, functioning with db

--- server.js
+++ server.js
@@ -12,6 +12,26 @@ MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
         console.log(`Hey, connected to ${dbName} database!`)
         db = client.db(dbName)
     })
+    .catch(err => {
+        console.log(err)
+    })
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+app.post("/createItem", (req, res) => {
+    db.collection("inventory").insertOne({item: req.body.inventoryItem, completed: false})
+    .then(result => {
+        console.log("An item has been added!")
+        res.redirect("/")
+    })
+})
 
 app.listen(PORT, () => {
     console.log("Server is running, better catch it!")
