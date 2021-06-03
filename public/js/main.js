8daf35d0d55ffe31f7a7feea9f4c6c458ec0adb2
Coby Leavitt <coby.leavitt@gmail.com>
2021-06-03 12:11:13 -0600

    added marking off items as complete/incomplete, added css dir and file

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,17 @@
 const deleteBtn = document.querySelectorAll(".del")
+const inventoryItem = document.querySelectorAll(".inventoryItem span")
+const itemComplete = document.querySelectorAll(".inventoryItem span.completed")
 
 Array.from(deleteBtn).forEach((el) => {
     el.addEventListener("click", deleteItem)
+})
+
+Array.from(inventoryItem).forEach((el) => {
+    el.addEventListener("click", completeItem)
+})
+
+Array.from(itemComplete).forEach((el) => {
+    el.addEventListener("click", undo)
 })
 
 async function deleteItem() {
@@ -9,6 +19,42 @@ async function deleteItem() {
     try{
         const response = await fetch("deleteItem", {
             method: "delete",
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify({
+                "rainbow": itemText
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function completeItem() {
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch("completeItem", {
+            method: "put",
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify({
+                "rainbow": itemText
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function undo() {
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch("undo", {
+            method: "put",
             headers: {"Content-type": "application/json"},
             body: JSON.stringify({
                 "rainbow": itemText
--- server.js
+++ server.js
@@ -21,26 +21,49 @@ app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    db.collection("inventory").find().toArray()
-    .then(data => {
-        res.render("index.ejs", {unicorn: data})
-    })
+app.get("/", async (req, res) => {
+    const todoItems = await db.collection("inventory").find().toArray()
+    const itemsLeft = await db.collection("inventory").countDocuments({completed: false})
+    res.render("index.ejs", {unicorn: todoItems, left: itemsLeft})
 })
 
 app.post("/createItem", (req, res) => {
     db.collection("inventory").insertOne({item: req.body.inventoryItem, completed: false})
     .then(result => {
-        console.log("An item has been added!")
+        console.log("Added item!")
         res.redirect("/")
+    })
+})
+
+app.put("/completeItem", (req, res) => {
+    db.collection("inventory").updateOne({item: req.body.rainbow}, {
+        $set: {
+            completed: true
+        }
+    })
+    .then(result => {
+        console.log("Completed item!")
+        res.json("Marked Complete")
+    })
+})
+
+app.put("/undo", (req, res) => {
+    db.collection("inventory").updateOne({item: req.body.rainbow}, {
+        $set: {
+            completed: false
+        }
+    })
+    .then(result => {
+        console.log("Undo completion of item!")
+        res.json("Marked Incomplete")
     })
 })
 
 app.delete("/deleteItem", (req, res) => {
     db.collection("inventory").deleteOne({item: req.body.rainbow})
     .then(result => {
-        console.log("Deleted dat item")
-        res.json("Deleted It")
+        console.log("Deleted item!")
+        res.json("Deleted Item")
     })
     .catch(err => console.log(err))
 })
