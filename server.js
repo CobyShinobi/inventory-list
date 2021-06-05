f705aafea17e11ce2b486b70d4ed3760e794cca1
Coby Leavitt <coby.leavitt@gmail.com>
2021-06-05 12:57:39 -0600

    setup mvc dirs and prepared server file

--- server.js
+++ server.js
@@ -1,74 +1,82 @@
 const express = require("express")
 const app = express()
-const MongoClient = require("mongodb").MongoClient
-const PORT = 1337
-require("dotenv").config()
+const connectDB = require("./config/database")
+const homeRoutes = require("./routes/home")
+const inventoryRoutes = require("./routes/inventory")
+// const MongoClient = require("mongodb").MongoClient
+// const PORT = 1337
+require("dotenv").config({path: "./config/.env"})
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = "inventory"
+connectDB()
 
-MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
-    .then(client => {
-        console.log(`Hey, connected to ${dbName} database!`)
-        db = client.db(dbName)
-    })
-    .catch(err => {
-        console.log(err)
-    })
+// let db,
+//     dbConnectionStr = process.env.DB_STRING,
+//     dbName = "inventory"
+
+// MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
+//     .then(client => {
+//         console.log(`Hey, connected to ${dbName} database!`)
+//         db = client.db(dbName)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.get("/", async (req, res) => {
-    const todoItems = await db.collection("inventory").find().toArray()
-    const itemsLeft = await db.collection("inventory").countDocuments({completed: false})
-    res.render("index.ejs", {unicorn: todoItems, left: itemsLeft})
-})
+app.use("/", homeRoutes)
+app.use("/inventory", inventoryRoutes)
 
-app.post("/createItem", (req, res) => {
-    db.collection("inventory").insertOne({item: req.body.inventoryItem, completed: false})
-    .then(result => {
-        console.log("Added item!")
-        res.redirect("/")
-    })
-})
+// app.get("/", async (req, res) => {
+//     const todoItems = await db.collection("inventory").find().toArray()
+//     const itemsLeft = await db.collection("inventory").countDocuments({completed: false})
+//     res.render("index.ejs", {unicorn: todoItems, left: itemsLeft})
+// })
 
-app.put("/completeItem", (req, res) => {
-    db.collection("inventory").updateOne({item: req.body.rainbow}, {
-        $set: {
-            completed: true
-        }
-    })
-    .then(result => {
-        console.log("Completed item!")
-        res.json("Marked Complete")
-    })
-})
+// app.post("/createItem", (req, res) => {
+//     db.collection("inventory").insertOne({item: req.body.inventoryItem, completed: false})
+//     .then(result => {
+//         console.log("Added item!")
+//         res.redirect("/")
+//     })
+// })
 
-app.put("/undo", (req, res) => {
-    db.collection("inventory").updateOne({item: req.body.rainbow}, {
-        $set: {
-            completed: false
-        }
-    })
-    .then(result => {
-        console.log("Undo completion of item!")
-        res.json("Marked Incomplete")
-    })
-})
+// app.put("/completeItem", (req, res) => {
+//     db.collection("inventory").updateOne({item: req.body.rainbow}, {
+//         $set: {
+//             completed: true
+//         }
+//     })
+//     .then(result => {
+//         console.log("Completed item!")
+//         res.json("Marked Complete")
+//     })
+// })
 
-app.delete("/deleteItem", (req, res) => {
-    db.collection("inventory").deleteOne({item: req.body.rainbow})
-    .then(result => {
-        console.log("Deleted item!")
-        res.json("Deleted Item")
-    })
-    .catch(err => console.log(err))
-})
+// app.put("/undo", (req, res) => {
+//     db.collection("inventory").updateOne({item: req.body.rainbow}, {
+//         $set: {
+//             completed: false
+//         }
+//     })
+//     .then(result => {
+//         console.log("Undo completion of item!")
+//         res.json("Marked Incomplete")
+//     })
+// })
 
-app.listen(process.env.PORT || PORT, () => {
+// app.delete("/deleteItem", (req, res) => {
+//     db.collection("inventory").deleteOne({item: req.body.rainbow})
+//     .then(result => {
+//         console.log("Deleted item!")
+//         res.json("Deleted Item")
+//     })
+//     .catch(err => console.log(err))
+// })
+
+app.listen(process.env.PORT, () => {
     console.log("Server is running, better catch it!")
 })
