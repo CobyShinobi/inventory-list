91f63f6fc722ad9fe32f6a40fba3e35edd508324
Coby Leavitt <coby.leavitt@gmail.com>
2021-06-06 20:10:30 -0600

    BROKEN. Set up database handling, MVC, and routes

---
+++ models/item.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+const ItemSchema = new mongoose.Schema({
+    item: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("Item", ItemSchema)
